36603ae5452a404c662e19796f33a0fe3f6342ea
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-01-01 15:54:39 +0100

    recipe detail part I

--- frontend/src/app/recipe-form/recipe-form.component.ts
+++ frontend/src/app/recipe-form/recipe-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { RecipeService } from '@app/services/recipe.service';
 import { Router } from '@angular/router';
@@ -6,12 +7,17 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
-  styleUrls: ['./recipe-form.component.css']
+  styleUrls: ['./recipe-form.component.css'],
 })
 export class RecipeFormComponent implements OnInit {
   recipeForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private recipeService: RecipeService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private recipeService: RecipeService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
     // Initialize the form group with all necessary form controls
     this.recipeForm = this.fb.group({
       name: [''], // Recipe name
@@ -38,7 +44,7 @@ export class RecipeFormComponent implements OnInit {
     const uniqueId = new Date().getTime();
     this.ingredients.push(
       this.fb.group({
-        id:[uniqueId],
+        id: [uniqueId],
         name: [''],
         amount: [''],
         unit: [''],
@@ -47,27 +53,32 @@ export class RecipeFormComponent implements OnInit {
   }
 
   // Method to remove an ingredient from the form array
- removeIngredientById(index: number): void{
-  const addedIngredientsById = this.recipeForm.get('ingredientsAdded') as FormArray;
-  if (index >= 0 && index < addedIngredientsById.length) {
-    addedIngredientsById.removeAt(index);
+  removeIngredientById(index: number): void {
+    const addedIngredientsById = this.recipeForm.get(
+      'ingredientsAdded'
+    ) as FormArray;
+    if (index >= 0 && index < addedIngredientsById.length) {
+      addedIngredientsById.removeAt(index);
+    }
   }
- }
 
   // Method to add an ingredient to the 'ingredientsAdded' array
   addIngredientToForm(index: number): void {
     const selectedIngredient = this.ingredients.at(index).value;
-    const addedIngredients = this.recipeForm.get('ingredientsAdded') as FormArray;
-    addedIngredients.push(this.fb.group({
-      index: [index],
-      name: [selectedIngredient.name],
-      amount: [selectedIngredient.amount],
-      unit: [selectedIngredient.unit],
-    }));
+    const addedIngredients = this.recipeForm.get(
+      'ingredientsAdded'
+    ) as FormArray;
+    addedIngredients.push(
+      this.fb.group({
+        index: [index],
+        name: [selectedIngredient.name],
+        amount: [selectedIngredient.amount],
+        unit: [selectedIngredient.unit],
+      })
+    );
 
     // Optional: Clear the ingredient fields after adding
     this.ingredients.at(index).reset();
-  
   }
 
   // Method to handle form submission
@@ -75,13 +86,31 @@ export class RecipeFormComponent implements OnInit {
     console.log('Form Submitted:', this.recipeForm.value);
     // Add your submission logic here
     this.recipeService.addNewRecipe(this.recipeForm.value).subscribe(
-      response => {
-console.log("Recipe added successfully", response);
-this.router.navigate(['/recipes']);
+      (response) => {
+        console.log('Recipe added successfully', response);
+        // Display success toast
+        this.snackBar.open('Recipe added successfully!', 'Close', {
+          duration: 3000, // Duration in milliseconds
+          horizontalPosition: 'center', // 'start' | 'center' | 'end' | 'left' | 'right'
+          verticalPosition: 'bottom', // 'top' | 'bottom'
+          panelClass: ['mat-toolbar', 'mat-primary'], // Styling
+        });
+
+        // Navigate after a slight delay to allow the user to see the message
+        setTimeout(() => {
+          this.router.navigate(['/recipes']);
+        }, 3200);
       },
-      error => {
-        console.log("Error adding recipe", error)
+      (error) => {
+        console.log('Error adding recipe', error);
+         // Display error toast
+         this.snackBar.open('Error adding recipe', 'Close', {
+          duration: 3000, // Duration in milliseconds
+          horizontalPosition: 'center', // 'start' | 'center' | 'end' | 'left' | 'right'
+          verticalPosition: 'bottom', // 'top' | 'bottom'
+          panelClass: ['mat-toolbar', 'mat-primary'], // Styling
+        });
       }
-    )
+    );
   }
 }
