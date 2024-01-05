5a65fd012eb832db867f019053a39d26fcd7d95f
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-01-05 15:49:05 +0100

    inventory detail delete button

--- frontend/src/app/inventory-detail/inventory-detail.component.ts
+++ frontend/src/app/inventory-detail/inventory-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import { InventoryService } from '@app/services/inventory.service';
 
 @Component({
   selector: 'app-inventory-detail',
   templateUrl: './inventory-detail.component.html',
-  styleUrls: ['./inventory-detail.component.css']
+  styleUrls: ['./inventory-detail.component.css'],
+  providers: [DatePipe], 
 })
 export class InventoryDetailComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private datePipe: DatePipe, private inventoryService: InventoryService) {}
+
+  formatExpirationDate(): string {
+    return this.datePipe.transform(this.data.expirationDate, 'dd.MM.yyyy')!;
+  }
+
+  deleteItemById(index: number): void {
+
+
+
+
+    this.inventoryService.deleteInventoryItemById(itemIdToDelete).subscribe(
+      () => {
+        console.log('Item deleted from the database');
+        // Additional logic if needed
+      },
+      (error) => {
+        console.error('Error deleting item from the database', error);
+        // Handle errors as needed
+      }
+  }
 
 }
+
