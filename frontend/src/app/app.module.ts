80026e64c3e794ee824bd1302be2833c1c44aaa3
pgrena <p.jakubow@octopus-news.com>
2024-01-24 10:53:31 +0100

    Update app.module.ts
    Added another Taiga modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,3 @@
-import { TuiRootModule, TuiDialogModule, TuiAlertModule } from "@taiga-ui/core";
-import {TuiInputModule} from '@taiga-ui/kit';
-import {TuiButtonModule} from '@taiga-ui/core';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,12 +8,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AddItemsComponent } from './inventory/add-items/add-items.component';
+
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiNotificationModule } from "@taiga-ui/core";
+import {TuiInputModule, TuiInputNumberModule, TuiSelectModule  } from '@taiga-ui/kit';
+import { TuiButtonModule } from '@taiga-ui/core';
+import {TuiTableModule} from '@taiga-ui/addon-table';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    AddItemsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +33,11 @@ import { RegisterComponent } from './auth/register/register.component';
       TuiInputModule,
       FormsModule,
       ReactiveFormsModule,
-      TuiButtonModule
+      TuiButtonModule,
+      TuiInputNumberModule,
+      TuiSelectModule,
+      TuiNotificationModule,
+      TuiTableModule
 ],
   providers: [],
   bootstrap: [AppComponent]
