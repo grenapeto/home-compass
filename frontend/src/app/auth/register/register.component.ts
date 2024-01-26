6a56502ff93132693300f3b86065cebe2c09abd0
grenapeto <grenapeto@gmail.com>
2024-01-26 14:35:23 +0100

    Update register.component.ts

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -9,45 +9,55 @@ import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
-constructor(private authService: AuthService, private router: Router) {}
+  registerForm = new FormGroup({
+    nameInput: new FormControl('', Validators.required),
+    usernameInput: new FormControl('', Validators.required),
+    emailInput: new FormControl('', [Validators.required, Validators.email]),
+    passwordInput: new FormControl('', Validators.required),
+    repeatPasswordInput: new FormControl('', Validators.required),
+  });
 
-registerForm = new FormGroup({
-  nameInput: new FormControl('', Validators.required),
-  usernameInput: new FormControl('', Validators.required),
-  emailInput: new FormControl('', [Validators.required, Validators.email]),
-  passwordInput: new FormControl('', Validators.required),
-  repeatPasswordInput: new FormControl('', Validators.required),
-})
+  successNotification = false;
+  errorNotification = false;
 
-successNotification = false;
-errorNotification = false;
+  register(): void {
+    
+    // Mark all form controls as touched to trigger validation messages
+    for (const controlName in this.registerForm.controls) {
+      if (this.registerForm.controls.hasOwnProperty(controlName)) {
+        this.registerForm.get(controlName)?.markAsTouched();
+      }
+    }
 
-register() : void {
-  const username = this.registerForm.get('usernameInput')?.value ?? '';
-  const email = this.registerForm.get('emailInput')?.value ?? '';
-  const password = this.registerForm.get('passwordInput')?.value ?? '';
+    if (this.registerForm.valid) {
+      const username = this.registerForm.get('usernameInput')?.value ?? '';
+      const email = this.registerForm.get('emailInput')?.value ?? '';
+      const password = this.registerForm.get('passwordInput')?.value ?? '';
 
-this.authService.registerUser(username, email, password).subscribe(
-  response => {
-    // Handle successful registration here (e.g., show a success message)
-    console.log('Registration successful', response);
-    this.successNotification = true;
-    timer(1000).pipe(
-      take(1) // Optionally, take only one emission
-    ).subscribe(() => {
-      this.router.navigate(['/login']);
-    });
- 
-  },
-  error => {
-    // Handle registration error here (e.g., show an error message)
-    console.error('Registration failed', error);
-    this.errorNotification = true;
+      this.authService.registerUser(username, email, password).subscribe(
+        (response) => {
+          // Handle successful registration here (e.g., show a success message)
+          console.log('Registration successful', response);
+          this.successNotification = true;
+          timer(1000)
+            .pipe(
+              take(1) // Optionally, take only one emission
+            )
+            .subscribe(() => {
+              this.router.navigate(['/login']);
+            });
+        },
+        (error) => {
+          // Handle registration error here (e.g., show an error message)
+          console.error('Registration failed', error);
+          this.errorNotification = true;
+        }
+      );
+    }
   }
-);
-}
 }
