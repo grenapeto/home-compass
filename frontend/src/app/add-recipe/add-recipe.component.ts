0bd03ae1d3665881211bd40671d588669c673279
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-06 14:06:54 +0100

    add recipe

--- frontend/src/app/add-recipe/add-recipe.component.ts
+++ frontend/src/app/add-recipe/add-recipe.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 import { RecipesService } from '../services/recipes.service';
 import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-recipe',
@@ -17,11 +18,42 @@ export class AddRecipeComponent implements OnInit {
   ) {
     this.addRecipeForm = this.fb.group({
       title: ['', Validators.required],
-      ingredients: ['', Validators.required],
-      instructions: ['', Validators.required],
+      ingredients: this.fb.array([this.createIngredient()]),
+      instructions: this.fb.array([this.createInstruction()]),
       // recipes: this.fb.array([]), // If you have an array of recipes
     });
   }
+
+ingredients(): FormArray {
+  return this.addRecipeForm.get('ingredients') as FormArray;
+}
+
+instructions(): FormArray {
+  return this.addRecipeForm.get('instructions') as FormArray;
+}
+
+createIngredient(): FormGroup {
+  return this.fb.group({
+    name: ['', Validators.required],
+    amount: ['', Validators.required],
+    unit: ['', Validators.required],
+  })
+}
+createInstruction(): FormGroup {
+  return this.fb.group({
+    instructions: ['']
+  })
+}
+addIngredient(): void {
+  this.ingredients().push(this.createIngredient());
+  console.log(this.ingredients())
+}
+
+addInstruction(): void {
+  this.instructions().push(this.createInstruction());
+  console.log(this.instructions())
+}
+
 
   ngOnInit(): void {}
 
