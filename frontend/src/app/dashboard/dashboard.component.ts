6451e4b7d87213fba5781a007e2633d8254bbede
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-08 09:52:44 +0100

    buttons

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { RecipesService } from '../services/recipes.service';
 import { InventoryService } from '../services/inventory.service';
-import { OnInit } from '@angular/core';
+import { tuiTablePaginationOptionsProvider } from '@taiga-ui/addon-table';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [tuiTablePaginationOptionsProvider({ showPages: false })],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private recipesService: RecipesService, private inventoryService: InventoryService) {}
-
   readonly columns = ['name', 'cookTime', 'actionbuttons'];
-
   recipesData: any;
+
+  readonly expColumns = ['name', 'expirationDate', 'amount'];
+  expData: any;
+
+  page = 3;
+  size = 10;
+
+  order = new Map<number, number>();
+
+  constructor(private recipesService: RecipesService, private inventoryService: InventoryService) {}
 
   ngOnInit(): void {
     this.recipesService.getAllRecipes().subscribe(
@@ -24,8 +33,20 @@ export class DashboardComponent implements OnInit {
       (error) => {
         console.log('no recipes', error);
       }
-    )
+    );
+
+    this.inventoryService.getAllInventoryItems().subscribe(
+      (response) => {
+        console.log('inventory', response);
+        this.expData = response; 
+      },
+      (error) => {
+        console.log('no inventory', error);
+      }
+    );
   }
+
+  
 
   items = [
     { w: 2, h: 4, content: 'Latest recipes', type: 'recipes' },
@@ -37,8 +58,5 @@ export class DashboardComponent implements OnInit {
     {w: 1, h: 1, content: 'Item 7'},
     {w: 1, h: 1, content: 'Item 8'},
     {w: 1, h: 1, content: 'Item 9'},
-];
-
-order = new Map<number, number>();
-
+  ];
 }
