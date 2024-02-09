e95279b399d394aab6adcab7451930a5b26d2b37
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-02-09 15:54:56 +0100

    mealplan

--- frontend/src/app/mealplans/add-mealplan/add-mealplan.component.ts
+++ frontend/src/app/mealplans/add-mealplan/add-mealplan.component.ts
@@ -25,13 +25,20 @@ export class AddMealplanComponent {
 
   hoveredItem: TuiDay | null = null;
 
+  mealplanDates: Date[] = []
+
   readonly markerHandler: TuiMarkerHandler = (day: TuiDay) =>
     // Attention: do not create new arrays in handler, use constants instead
     day.day % 2 === 0 ? TWO_DOTS : ONE_DOT;
 
+    constructor() {
+      const currentDay = TuiDay.currentLocal();
+      this.updateDates(currentDay);
+    }
+
   onDayClick(day: TuiDay): void {
     this.value = day;
-    console.log(this.value)
+    this.updateDates(day);
   }
 
   onMonthChangeFirst(month: TuiMonth): void {
@@ -50,5 +57,15 @@ export class AddMealplanComponent {
     this.firstMonth = month.append({ month: -2 });
     this.middleMonth = month.append({ month: -1 });
     this.lastMonth = month;
+  }
+
+  private updateDates(tuiDay: TuiDay): void {
+    const selectedDate = new Date(tuiDay.year, tuiDay.month-1, tuiDay.day)
+    this.mealplanDates = [];
+    for (let i = -2; i <= 2; i++){
+      let date = new Date(selectedDate)
+      date.setDate(selectedDate.getDate()+i)
+      this.mealplanDates.push(date)
+    };
   }
 }
--- frontend/src/app/mealplans/mealplan-detail/mealplan-detail.component.ts
+++ frontend/src/app/mealplans/mealplan-detail/mealplan-detail.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component ,Input, OnChanges, SimpleChanges, input } from '@angular/core';
 
 @Component({
   selector: 'app-mealplan-detail',
   templateUrl: './mealplan-detail.component.html',
   styleUrl: './mealplan-detail.component.css'
 })
-export class MealplanDetailComponent {
-  currentDate: Date;
-  currentDayName: string;
+export class MealplanDetailComponent implements OnChanges {
+  
+@Input() currentDate!: Date;
 
-  constructor() {
-    this.currentDate = new Date();
-    this.currentDayName = this.getCurrentDayName();
-  }
+currentDayName: string = '';
+previousDays: Date[] = [];
+followingDays: Date[] = [];
 
-  getCurrentDayName(): string {
-    const date = new Date();
-    return date.toLocaleDateString('en-US', { weekday: 'long'})
-  }
+ngOnChanges(changes: SimpleChanges): void {
+  if (changes['currentDate'] && this.currentDate){
+    this.updateDays();
+  };
+}
+
+private updateDays(): void {
+  this.currentDayName = this.currentDate.toLocaleDateString('en-US', { weekday: 'long'})
+  this.previousDays = this.calculateDays(-2, -1);
+  this.followingDays = this.calculateDays(1, 2);
+}
+
+private calculateDays(startOffset: number, endOffset: number): Date[] {
+ const dates: Date[] = [];
+ for (let i = startOffset; i <= endOffset; i++){
+  let date = new Date(this.currentDate);
+  date.setDate(this.currentDate.getDate()+i);
+  dates.push(date);
+ }
+ return dates;
+}
+
 }
